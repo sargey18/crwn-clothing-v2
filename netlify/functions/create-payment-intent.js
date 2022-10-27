e192516107fa38de6c75f73a0bfebae63da1f274
Ryan Sargeson <ryanteachviet@gmail.com>
2022-10-27 14:12:52 +0100

    sign in working

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,28 +1,26 @@
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-// test six
 exports.handler = async (event) => {
-    try {
-        
-        const {amount} = JSON.parse(event.body);
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount,
-            currency: "usd",
-            payment_method_types: ["card"]
-        });
+  try {
+    const { amount } = JSON.parse(event.body);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({paymentIntent})
-        }
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
 
-    } catch (error) {
-        console.log({error});
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
 
-        return {
-            status: 400,
-            body: JSON.stringify({error}),
-        }
-    }
-}
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
